a54efd02b292947ebeb822e4c9589fa33a1aecc0
VJ-Duardo <57729882+VJ-Duardo@users.noreply.github.com>
2021-12-05 15:47:40 +0100

    day 3 part 2

--- Day3/Binary.cs
+++ Day3/Binary.cs
@@ -5,12 +5,29 @@ namespace aoc.Day3
 {
     class Binary
     {
+        enum Rating
+        {
+            OxygenGeneratorRating,
+            Co2ScrubberRating
+        }
 
         static void Main()
         {
             string[] diagnostic = Resources.day3_input.Split("\r\n");
 
             Console.WriteLine(GetPowerConsumption(diagnostic));
+            Console.WriteLine(GetLifeSupportRating(diagnostic));
+        }
+
+
+        static int GetMostCommonBit(string[] arr, int pos)
+        {
+            int occStatus = 0;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                occStatus = arr[i][pos] == '0' ? occStatus - 1 : occStatus + 1;
+            }
+            return occStatus < 0 ? 0 : 1;
         }
 
 
@@ -19,16 +36,31 @@ namespace aoc.Day3
             int digits = report[0].Length;
             int initial = (int)(Math.Pow(2, digits) - 1);
             int number = initial;
-            for(int i = 0; i < digits; i++)
+            for (int i = 0; i < digits; i++)
             {
-                int occStatus = 0;
-                for (int j = 0; j < report.Length; j++)
-                {
-                    occStatus = report[j][i] == '0' ? occStatus - 1 : occStatus + 1;
-                }
-                number = occStatus < 0 ? ~(~number | (1 << digits-(i+1))) : number;
+                number = GetMostCommonBit(report, i) == 0 ? ~(~number | (1 << digits - (i + 1))) : number;
             }
             return number * (~number & initial);
+        }
+
+
+        static int DetermineLastBinary(string[] report, int index, Rating rating)
+        {
+            if (report.Length == 1)
+            {
+                return Convert.ToInt32(report[0], 2);
+            }
+
+            int occStatus = GetMostCommonBit(report, index);
+            occStatus = rating == Rating.Co2ScrubberRating ? 1 - occStatus : occStatus;
+
+            return DetermineLastBinary(Array.FindAll(report, s => s[index] == (char)(occStatus + '0')), index + 1, rating);
+        }
+
+
+        static int GetLifeSupportRating(string[] report)
+        {
+            return DetermineLastBinary(report, 0, Rating.OxygenGeneratorRating) * DetermineLastBinary(report, 0, Rating.Co2ScrubberRating);
         }
 
     }
