cbccb47b5805c1cb6057e6ba3c6e68f6ddedc348
VJ-Duardo <olafstein501@gmail.com>
2021-12-07 11:25:18 +0100

    day 4

---
+++ Day4/Bingo.cs
@@ -0,0 +1,75 @@
+﻿using aoc.Properties;
+using System;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace aoc.Day4
+{
+    class Bingo
+    {
+        static void Main()
+        {
+            string[] parts = Resources.day4_input.Split("\r\n\r\n");
+            int[] draws = Array.ConvertAll(parts[0].Split(","), s => Int32.Parse(s));
+
+            Board[] boards = GetBoards(parts);
+
+            Console.WriteLine(GetFinalScore(boards, draws));
+            Console.WriteLine(GetFinalScoreLoser(boards, draws));
+        }
+
+
+        static int GetFinalScore(Board[] boards, int[] draws)
+        {
+            for (int i=0; i< draws.Length; i++)
+            {
+                for (int j=0; j< boards.Length; j++)
+                {
+                    boards[j].MarkNext(draws[i]);
+                    if (boards[j].TestForWin())
+                    {
+                        return boards[j].GetUnmarkedSum()*draws[i];
+                    }
+                }
+            }
+            return -1;
+        }
+
+        static int GetFinalScoreLoser(Board[] boards, int[] draws)
+        {
+            int amount = boards.Length;
+            for (int i = 0; i < draws.Length; i++)
+            {
+                for (int j = 0; j < boards.Length; j++)
+                {
+                    boards[j].MarkNext(draws[i]);
+                    if (!boards[j].GetDone() && boards[j].TestForWin())
+                    {
+                        if (amount == 1)
+                        {
+                            return boards[j].GetUnmarkedSum() * draws[i];
+                        }
+                        boards[j].SetDone();
+                        amount--;
+                    }
+                }
+            }
+            return -1;
+        }
+
+
+        static Board[] GetBoards(string[] parts)
+        {
+            Board[] boards = new Board[parts.Length-1];
+            for (int i=1; i < parts.Length; i++)
+            {
+                boards[i - 1] = new Board(parts[i]
+                    .Split("\r\n")
+                    .Select(line => Array.ConvertAll(Regex.Split(line.Trim(), @"\s+"), s => { return (Int32.Parse(s), false); }))
+                    .ToArray());
+            }
+            return boards;
+        }
+        
+    }
+}
---
+++ Day4/Board.cs
@@ -0,0 +1,104 @@
+﻿using System;
+
+namespace aoc.Day4
+{
+    class Board
+    {
+        private (int val, bool marked)[][] numbers;
+        private readonly int width;
+        private readonly int height;
+        private bool done = false;
+
+        public Board((int val, bool marked)[][] numbers)
+        {
+            this.numbers = numbers;
+            this.width = this.numbers[0].Length;
+            this.height = this.numbers.Length;
+        }
+
+
+        public void MarkNext(int next)
+        {
+            for (int i=0; i < this.height; i++)
+            {
+                for (int j = 0; j < this.width; j++)
+                {
+                    if (this.numbers[i][j].val == next)
+                    {
+                        this.numbers[i][j].marked = true;
+                    }
+                }
+            }
+        }   
+
+
+        public int GetUnmarkedSum()
+        {
+            int sum = 0;
+            for (int i = 0; i < this.height; i++)
+            {
+                for (int j = 0; j < this.width; j++)
+                {
+                    if (!this.numbers[i][j].marked)
+                    {
+                        sum += this.numbers[i][j].val;
+                    }
+                }
+            }
+            return sum;
+        }
+
+
+        public bool TestForWin()
+        {
+            return this.TestLines() || this.TestColumns();
+        }
+
+        private bool TestLines()
+        {
+            return Array.Exists(this.numbers, line => 
+            {
+                for (int i=0; i < this.height; i++)
+                {
+                    if (!line[i].marked)
+                    {
+                        return false;
+                    }
+                }
+                return true;
+            });
+        }
+
+        private bool TestColumns()
+        {
+            for (int j=0; j < this.width; j++)
+            {
+                bool win = true;
+                for (int i=0; i < this.height; i++)
+                {
+                    if (!this.numbers[i][j].marked)
+                    {
+                        win = false;
+                        break;
+                    }
+                }
+                if (win)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+
+        public void SetDone()
+        {
+            this.done = true;
+        }
+
+        public bool GetDone()
+        {
+            return this.done;
+        }
+    }
+}
