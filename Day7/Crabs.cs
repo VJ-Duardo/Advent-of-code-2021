30914dbde360bf93a8641f6b1f2775cdd2ffef05
VJ-Duardo <olafstein501@gmail.com>
2021-12-09 13:07:50 +0100

    day 7

---
+++ Day7/Crabs.cs
@@ -0,0 +1,46 @@
+﻿using aoc.Properties;
+using System;
+using System.Linq;
+
+namespace aoc.Day7
+{
+    class Crabs
+    {
+
+        static void Main()
+        {
+            int[] positions = Array.ConvertAll(Resources.day7_input.Split(","), s => Int32.Parse(s));
+            Console.WriteLine(GetTotalFuel(positions));
+            Console.WriteLine(GetRealTotalFuel(positions));
+        }
+
+
+        static int GetTotalFuel(int[] positions)
+        {
+            return positions.Select(pos =>
+            {
+                int diff = 0;
+                for (int i = 0; i < positions.Length; i++)
+                {
+                    diff += Math.Abs(pos - positions[i]);
+                }
+                return diff;
+            }).ToArray().Min();
+        }
+
+        static int GetRealTotalFuel(int[] positions)
+        {
+            return positions.Select(pos =>
+            {
+                int diff = 0;
+                for (int i = 0; i < positions.Length; i++)
+                {
+
+                    int distance = Math.Abs(pos - positions[i]);
+                    diff += (int)((Math.Pow(distance, 2) + distance) / 2);
+                }
+                return diff;
+            }).ToArray().Min();
+        }
+    }
+}
--- Properties/Resources.Designer.cs
+++ Properties/Resources.Designer.cs
@@ -344,11 +344,20 @@ namespace aoc.Properties {
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to .
+        ///   Looks up a localized string similar to 3,5,3,1,4,4,5,5,2,1,4,3,5,1,3,5,3,2,4,3,5,3,1,1,2,1,4,5,3,1,4,5,4,3,3,4,3,1,1,2,2,4,1,1,4,3,4,4,2,4,3,1,5,1,2,3,2,4,4,1,1,1,3,3,5,1,4,5,5,2,5,3,3,1,1,2,3,3,3,1,4,1,5,1,5,3,3,1,5,3,4,3,1,4,1,1,1,2,1,2,3,2,2,4,3,5,5,4,5,3,1,4,4,2,4,4,5,1,5,3,3,5,5,4,4,1,3,2,3,1,2,4,5,3,3,5,4,1,1,5,2,5,1,5,5,4,1,1,1,1,5,3,3,4,4,2,2,1,5,1,1,1,4,4,2,2,2,2,2,5,5,2,4,4,4,1,2,5,4,5,2,5,4,3,1,1,5,4,5,3,2,3,4,1,4,1,1,3,5,1,2,5,1,1,1,5,1,1,4,2,3,4,1,3,3,2,3,1,1,4,4,3,2,1,2,1,4,2,5,4,2,5,3,2,3,3,4,1,3,5,5,1,3,4,5,1,1,3,1,2,1,1,1,1,5,1, [rest of string was truncated]&quot;;.
         /// </summary>
         internal static string day6_input {
             get {
                 return ResourceManager.GetString("day6_input", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to .
+        /// </summary>
+        internal static string day7_input {
+            get {
+                return ResourceManager.GetString("day7_input", resourceCulture);
             }
         }
     }
